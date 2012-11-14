08a93cbc80f93b4ea87b1cc255be2bbc5444330f
needs <needs@mailoo.org>
2012-11-14 16:37:41 +0100

    Mise en forme de LecteurPhraseSimple

--- LecteurPhraseSimple.cc
+++ LecteurPhraseSimple.cc
@@ -1,23 +1,26 @@
-#include "LecteurPhraseSimple.h"
-
 #include <stdlib.h>
 #include <iostream>
 using namespace std;
 
-////////////////////////////////////////////////////////////////////////////////
+#include "LecteurPhraseSimple.h"
+
+
 LecteurPhraseSimple::LecteurPhraseSimple(string nomFich) :
-	ls(nomFich) {
+	ls(nomFich)
+{
 }
 
-////////////////////////////////////////////////////////////////////////////////
-void LecteurPhraseSimple::analyse() {
+
+void LecteurPhraseSimple::analyse()
+{
 	programme();
 	cout << "Syntaxe correcte." << endl;
 }
 
-////////////////////////////////////////////////////////////////////////////////
-void LecteurPhraseSimple::programme() {
+
 // <programme> ::= debut <seqInst> fin FIN_FICHIER
+void LecteurPhraseSimple::programme()
+{
 	
 	sauterSymCour("debut");
 	seqInst();
@@ -25,55 +28,55 @@ void LecteurPhraseSimple::programme() {
 	testerSymCour("<FINDEFICHIER>");
 }
 
-////////////////////////////////////////////////////////////////////////////////
-void LecteurPhraseSimple::seqInst() {
-// <seqInst> ::= <inst> ; { <inst> ; }
 
+// <seqInst> ::= <inst> ; { <inst> ; }
+void LecteurPhraseSimple::seqInst()
+{
 	do {
 		inst();
 		sauterSymCour(";");
-	} while (ls.getSymCour()=="<VARIABLE>");
+	} while (ls.getSymCour() == "<VARIABLE>");
 	// tant que le symbole courant est un debut possible d'instruction...
 }
 
-////////////////////////////////////////////////////////////////////////////////
-void LecteurPhraseSimple::inst() {
-// <inst> ::= <affectation>
 
+// <inst> ::= <affectation>
+void LecteurPhraseSimple::inst()
+{
 	affectation();
 }
 
-////////////////////////////////////////////////////////////////////////////////
-void LecteurPhraseSimple::affectation() {
-// <affectation> ::= <variable> = <expression>
 
+// <affectation> ::= <variable> = <expression>
+void LecteurPhraseSimple::affectation()
+{
 	sauterSymCour("<VARIABLE>");
 	sauterSymCour("=");
 	expression();
 }
 
-////////////////////////////////////////////////////////////////////////////////
-void LecteurPhraseSimple::expression() {
-// <expression> ::= <facteur> { <opBinaire> <facteur> }
 
+// <expression> ::= <facteur> { <opBinaire> <facteur> }
+void LecteurPhraseSimple::expression()
+{
 	facteur();
-	while (ls.getSymCour()=="+" || ls.getSymCour()=="-" || 
-		   ls.getSymCour()=="*" || ls.getSymCour()=="/") {
+	while (ls.getSymCour() == "+" || ls.getSymCour() == "-" || 
+	       ls.getSymCour() == "*" || ls.getSymCour() == "/") {
 		opBinaire();
 		facteur();
 	}
 }
 
-////////////////////////////////////////////////////////////////////////////////
-void LecteurPhraseSimple::facteur() {
-// <facteur> ::= <entier>  |  <variable>  |  - <facteur>  |  ( <expression> )
 
-	if (ls.getSymCour()=="<VARIABLE>" || ls.getSymCour()=="<ENTIER>")
+// <facteur> ::= <entier>  |  <variable>  |  - <facteur>  |  ( <expression> )
+void LecteurPhraseSimple::facteur()
+{
+	if (ls.getSymCour() == "<VARIABLE>" || ls.getSymCour() == "<ENTIER>")
 		ls.suivant();
-	else if (ls.getSymCour()=="-") {
+	else if (ls.getSymCour() == "-") {
 		ls.suivant();
 		facteur();
-	} else if (ls.getSymCour()=="(") {
+	} else if (ls.getSymCour() == "(") {
 		ls.suivant();
 		expression();
 		sauterSymCour(")");
@@ -81,38 +84,40 @@ void LecteurPhraseSimple::facteur() {
 		erreur("<facteur>");
 }
 
-////////////////////////////////////////////////////////////////////////////////
-void LecteurPhraseSimple::opBinaire()
+
 // <opBinaire> ::= + | - | *  | /
+void LecteurPhraseSimple::opBinaire()
 {
-	if (ls.getSymCour()=="+" || ls.getSymCour()=="-" || 
-		ls.getSymCour()=="*" || ls.getSymCour()=="/")
+	if (ls.getSymCour() == "+" || ls.getSymCour() == "-" || 
+	    ls.getSymCour() == "*" || ls.getSymCour() == "/")
 		ls.suivant();
 	else
 		erreur("<opBinaire>");
 }
 
-////////////////////////////////////////////////////////////////////////////////
+
 void LecteurPhraseSimple::testerSymCour(string ch) {
 	if (ls.getSymCour() != ch) {
 		cout << endl << "-------- Erreur ligne " << ls.getLigne()
-				<< " - Colonne " << ls.getColonne() << endl << "   Attendu : "
-				<< ch << endl << "   Trouve  : " << ls.getSymCour() << endl
-				<< endl;
+		     << " - Colonne " << ls.getColonne() << endl << "   Attendu : "
+		     << ch << endl << "   Trouve  : " << ls.getSymCour() << endl
+		     << endl;
 		exit(0); // plus tard, on levera une exception
 	}
 }
 
-////////////////////////////////////////////////////////////////////////////////
-void LecteurPhraseSimple::sauterSymCour(string ch) {
+
+void LecteurPhraseSimple::sauterSymCour(string ch)
+{
 	testerSymCour(ch);
 	ls.suivant();
 }
 
-////////////////////////////////////////////////////////////////////////////////
-void LecteurPhraseSimple::erreur(string mess) {
+
+void LecteurPhraseSimple::erreur(string mess)
+{
 	cout << endl << "-------- Erreur ligne " << ls.getLigne() << " - Colonne "
-			<< ls.getColonne() << endl << "   Attendu : " << mess << endl
-			<< "   Trouve  : " << ls.getSymCour() << endl << endl;
+	     << ls.getColonne() << endl << "   Attendu : " << mess << endl
+	     << "   Trouve  : " << ls.getSymCour() << endl << endl;
 	exit(0); // plus tard, on levera une exception
 }
