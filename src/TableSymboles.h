3784b359d4ad8111455bbf4644afad76fe90acac
needs <needs@mailoo.org>
2012-11-26 15:42:29 +0100

    Clarification des fichiers src/TableSymboles.*

--- src/TableSymboles.h
+++ src/TableSymboles.h
@@ -8,17 +8,17 @@ using namespace std;
 
 class TableSymboles {
 public:
-    TableSymboles(); // Construit une table vide de pointeurs sur des symboles valués
-    SymboleValue * chercheAjoute (Symbole s);
-      // si s est identique à un symbole valué déjà présent dans la table,
-      // on renvoie un pointeur sur ce symbole valué
-      // Sinon on insère un nouveau symbole valué correspondant à s
-      // et on renvoie un pointeur sur le nouveau symbole valué inséré
+	TableSymboles(); // Construit une table vide de pointeurs sur des symboles valués
+	SymboleValue* chercheAjoute (Symbole s);
+	// si s est identique à un symbole valué déjà présent dans la table,
+	// on renvoie un pointeur sur ce symbole valué
+	// Sinon on insère un nouveau symbole valué correspondant à s
+	// et on renvoie un pointeur sur le nouveau symbole valué inséré
 
-    friend ostream & operator << (ostream & cout, TableSymboles ts); // affiche ts sur cout
+	friend ostream & operator << (ostream & cout, TableSymboles ts); // affiche ts sur cout
 
 private:
-    vector<SymboleValue *> table; // la table des symboles valués, triée sur la chaine
+	vector<SymboleValue*> table; // la table des symboles valués, triée sur la chaine
 };
 
 #endif /* TABLESYMBOLES_H_ */
